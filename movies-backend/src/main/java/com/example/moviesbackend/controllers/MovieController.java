12479b9c4c14c430383e6b4b3efaff1ad3340664
Harshul Jain <harshulgangwal02@gmail.com>
2024-03-12 22:16:02 +0530

    Added Movie Repository

--- movies-backend/src/main/java/com/example/moviesbackend/controllers/MovieController.java
+++ movies-backend/src/main/java/com/example/moviesbackend/controllers/MovieController.java
@@ -1,5 +1,20 @@
 package com.example.moviesbackend.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/api/v1/movies")
 public class MovieController {
+    
+    @GetMapping()
+    public ResponseEntity<String> allMovies() {
+        return new ResponseEntity<String>("All Movies", HttpStatus.OK);
+    }
     
 }
---
+++ movies-backend/src/main/java/com/example/moviesbackend/dao/MovieRepository.java
@@ -0,0 +1,12 @@
+package com.example.moviesbackend.dao;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.moviesbackend.model.Movie;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId>{
+    
+}
