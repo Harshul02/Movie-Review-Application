c4bc03fbe4a69df4fb2194d8f91021d3e74379a7
Harshul Jain <harshulgangwal02@gmail.com>
2024-03-13 15:55:26 +0530

    Added Trailers

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
 
 function App() {
 
@@ -30,6 +31,7 @@ function App() {
     <Header />
       <Routes>
         <Route path='/' element={<Home movies={movies} />} />
+        <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
         <Route path='/layout' element={<Layout />} />
       </Routes>
     </BrowserRouter>
