68116eedc5b1479c4a1c263b1a3caa7727e71af5
Harshul Jain <harshulgangwal02@gmail.com>
2024-03-13 15:56:25 +0530

    Added cross origin

--- movies-backend/src/main/java/com/example/moviesbackend/controllers/ReviewController.java
+++ movies-backend/src/main/java/com/example/moviesbackend/controllers/ReviewController.java
@@ -12,12 +12,14 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
     
