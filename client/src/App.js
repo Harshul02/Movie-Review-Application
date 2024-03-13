ded0f130d04fc595b71de4a5d2cb8214b7397135
Harshul Jain <harshulgangwal02@gmail.com>
2024-03-13 16:58:14 +0530

    Added Page Not Found

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
 import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
 
@@ -49,7 +50,8 @@ function App() {
         <Route path='/' element={<Home movies={movies} />} />
         <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
         <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
-        <Route path='/layout' element={<Layout />} />
+        <Route path='*' element={<NotFound />} />
+        {/* <Route path='/layout' element={<Layout />} /> */}
       </Routes>
     </BrowserRouter>
     </div>
